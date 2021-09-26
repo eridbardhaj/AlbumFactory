171b6919b6de0cb3cdf5ccc5cb7107d44e15c947
Erid Bardhaj <erid.bardhaj@email.com>
2021-09-26 05:06:35 +0200

    Create an array extension to chunk arrays

---
+++ AlbumFactory/AlbumFactory/Utils/Extensions/Array+Generic.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+extension Array {
+    public func chunked(into size: Int) -> [[Element]] {
+        stride(from: 0, to: count, by: size)
+            .map { Array(self[$0 ..< Swift.min($0 + size, count)]) }
+    }
+}
