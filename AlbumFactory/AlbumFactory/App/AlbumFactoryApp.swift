2a274b4fdcdeb114de954d9b2e311dc081ddb7eb
Erid Bardhaj <erid.bardhaj@gmail.com>
2021-09-27 04:26:36 +0200

    Create plain screens for Home to display values

--- AlbumFactory/AlbumFactory/App/AlbumFactoryApp.swift
+++ AlbumFactory/AlbumFactory/App/AlbumFactoryApp.swift
@@ -1,10 +1,19 @@
 import SwiftUI
+import Alamofire
 
 @main
 struct AlbumFactoryApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                let session = Session()
+                let networkHandler = NetworkHandler(session: session)
+                let jsonDecoder = JSONDecoder()
+                let agent = NetworkAgent(networkHandler: networkHandler, jsonDecoder: jsonDecoder)
+                let networkAPI = NetworkKit(agent: agent)
+                let viewModel = HomeContentViewModel(networkAPI: networkAPI)
+                HomeContentView(viewModel: viewModel)
+            }
         }
     }
 }
