0eb4883dc10f2fd0f1fd32dbe42d5fe9a2e7b897
Erid Bardhaj <erid.bardhaj@email.com>
2021-09-27 04:24:11 +0200

    Use path correctly on NetworkRequest

--- AlbumFactory/AlbumFactory/Network/Internal/NetworkConfiguration.swift
+++ AlbumFactory/AlbumFactory/Network/Internal/NetworkConfiguration.swift
@@ -2,6 +2,7 @@ import Foundation
 
 enum NetworkConfiguration {
     static let scheme        = "http"
-    static let host          = "ws.audioscrobbler.com/2.0"
+    static let host          = "ws.audioscrobbler.com"
+    static let path          = "/2.0/"
     static let apiKey        = "a9fe56e3dd6e863a6011c31707d36564"
 }
