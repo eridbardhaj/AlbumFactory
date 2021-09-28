5a972bd7af97daab69d3bd2dfc8c69e5082e2d69
eridbardhaj <erid.bardhaj@gmail.com>
2021-09-28 23:20:11 +0200

    Add events of routing for HomeContentView

--- AlbumFactory/AlbumFactory/Features/Home/HomeContentView.swift
+++ AlbumFactory/AlbumFactory/Features/Home/HomeContentView.swift
@@ -1,26 +1,39 @@
 import SwiftUI
 
+protocol HomeContentViewDelegate: AnyObject {
+    func homeContentViewDidTapAlbum(album: Album)
+    func homeContentViewDidTapSearchButton()
+}
+
 struct HomeContentView: View {
 
     // MARK: - Properties
     // MARK: Mutable
 
     @ObservedObject private var viewModel: HomeContentViewModel
+    private weak var coordinatorDelegate: HomeContentViewDelegate?
 
     // MARK: - Initializers
 
-    init(viewModel: HomeContentViewModel) {
+    init(viewModel: HomeContentViewModel, coordinatorDelegate: HomeContentViewDelegate?) {
         self.viewModel = viewModel
+        self.coordinatorDelegate = coordinatorDelegate
     }
 
     // MARK: - View Configuration
 
     var body: some View {
         Group {
-            CollectionView(items: $viewModel.albums) { album, _ in
-                let viewModel = HomeContentItemViewModel(album: album)
-                HomeContentItemView(viewModel: viewModel)
-            }
+            CollectionView(
+                items: $viewModel.albums,
+                tapAction: { album, _ in
+                    coordinatorDelegate?.homeContentViewDidTapAlbum(album: album)
+                },
+                itemBuilder: { album, _ in
+                    let viewModel = HomeContentItemViewModel(album: album)
+                    HomeContentItemView(viewModel: viewModel)
+                }
+            )
             .frame(maxWidth: .infinity, maxHeight: .infinity)
             .navigationTitle("Albums")
         }
@@ -31,6 +44,6 @@ struct HomeContentView: View {
 struct HomeContentView_Previews: PreviewProvider {
     static var previews: some View {
         let viewModel = HomeContentViewModel(networkAPI: NetworkAPIMock())
-        HomeContentView(viewModel: viewModel)
+        HomeContentView(viewModel: viewModel, coordinatorDelegate: nil)
     }
 }
